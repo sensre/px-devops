55c88128c2b3683835138d9504eaeeb160fe066a
sensre <sensri108@gmail.com>
2022-08-24 00:30:42 -0700

    Portworx Scripts and Development

--- py_scripts/reading_file.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-with open("example.txt") as file:
-    for line in file:
-        print(line.strip().upper())
--- py_scripts/write_file.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-with open("example.txt", "a") as file:
-    file.write("Adding this line and writing it from node")
