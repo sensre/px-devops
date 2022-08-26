cf2de5df315e85a81132d2b398d7c45de56b77c7
sensre <sensri108@gmail.com>
2022-08-26 00:59:32 -0700

    Portworx Scripts and Development

--- monitoring/grafana/setup-grafana.sh
+++ monitoring/grafana/setup-grafana.sh
@@ -1,6 +1,29 @@
-#!/bin/bash 
+#!/bin/bash
 
-kubectl apply -f secret-grafana-credentials.yaml
-kubectl apply -f configmap-px-grafana-dashboards.yaml
-kubectl apply -f deployment-grafana.yaml
-kubectl apply -f service-grafana.yaml
+kubectl apply -f service-monitor.yaml
+
+kubectl -n kube-system create configmap grafana-dashboard-config --from-file=grafana-dashboard-config.yaml
+
+kubectl -n kube-system create configmap grafana-source-config --from-file=grafana-datasource.yaml
+
+
+curl "https://docs.portworx.com/samples/k8s/pxc/portworx-cluster-dashboard.json" -o portworx-cluster-dashboard.json && \
+curl "https://docs.portworx.com/samples/k8s/pxc/portworx-node-dashboard.json" -o portworx-node-dashboard.json && \
+curl "https://docs.portworx.com/samples/k8s/pxc/portworx-volume-dashboard.json" -o portworx-volume-dashboard.json && \
+curl "https://docs.portworx.com/samples/k8s/pxc/portworx-performance-dashboard.json" -o portworx-performance-dashboard.json && \
+curl "https://docs.portworx.com/samples/k8s/pxc/portworx-etcd-dashboard.json" -o portworx-etcd-dashboard.json && \
+kubectl -n kube-system create configmap grafana-dashboards --from-file=portworx-cluster-dashboard.json --from-file=portworx-performance-dashboard.json --from-file=portworx-node-dashboard.json --from-file=portworx-volume-dashboard.json --from-file=portworx-etcd-dashboard.json
+
+
+
+kubectl apply -f grafana.yaml
+
+
+#kubectl apply -f secret-grafana-credentials.yaml
+
+#kubectl apply -f configmap-px-grafana-dashboards.yaml
+
+#kubectl apply -f deployment-grafana.yaml
+
+#kubectl apply -f service-grafana.yaml
+
--- monitoring/grafana/teardown-grafana.sh
+++ monitoring/grafana/teardown-grafana.sh
@@ -1,6 +1,11 @@
 #!/bin/bash 
 
-kubectl delete -f service-grafana.yaml
-kubectl delete -f deployment-grafana.yaml
-kubectl delete -f configmap-px-grafana-dashboards.yaml
-kubectl delete -f secret-grafana-credentials.yaml
+kubectl delete -f service-monitor.yaml
+
+kubectl -n kube-system delete configmap grafana-dashboard-config
+
+kubectl -n kube-system delete configmap grafana-source-config
+
+kubectl -n kube-system delete configmap grafana-dashboards
+
+kubectl delete -f grafana.yaml
